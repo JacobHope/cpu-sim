44cfb011a88b1e41495628904c45286c5cb48038
Jacob M. Hope <jacobmhope@gmail.com>
2019-04-20 16:30:23 -0500

    Remove outlet connections in decode view controller.

---
+++ CPUSim/CPUSim/View/TouchTabView.swift
@@ -0,0 +1,101 @@
+//
+//  TouchBarView.swift
+//  CPUSim
+//
+//  Created by Jacob Morgan Hope on 4/20/19.
+//  Copyright © 2019 Jacob M. Hope. All rights reserved.
+//
+
+import Foundation
+
+class TouchBarView: UIView {
+    
+    var name: String = "touchBar"
+    
+    func setupWith(_ dotModel: DotModel) {
+        self.dotModel = dotModel
+        
+        // Generate a Pulsator instance
+        let pulsator = Pulsator.generatePulsator(radius: self.frame.width, backgroundColor: UIColor.blue.cgColor)
+        
+        // Add Pulsator as a sublayer to TouchPointView
+        self.layer.addSublayer(pulsator)
+        
+        // Start pulsating
+        pulsator.start()
+        
+        // Keep track of pulsator in order to change color, toggle off, etc
+        self.pulsator = pulsator
+        
+        // Setup dot
+        dot = CAShapeLayer.generateDotWith(
+            self.dotModel!,
+            color: UIColor.blue.cgColor)
+        
+        // Render dot
+        self.layer.addSublayer(dot!)
+    }
+    
+    func hitTest(_ touch: UITouch, event: UIEvent?) -> UIView? {
+        guard let hitView = self.hitTest(
+            CGPoint(x: touch.location(in: self).x + self.frame.width / 2,
+                    y: touch.location(in: self).y + self.frame.height / 2),
+            with: event)
+            else {
+                return nil
+        }
+        return hitView
+    }
+    
+    func setCorrect() {
+        // Change pulsator to green color
+        pulsator?.backgroundColor = UIColor.green.cgColor
+        
+        // Remove previous dot
+        dot?.removeFromSuperlayer()
+        
+        // Generate green dot
+        dot = CAShapeLayer.generateDotWith(
+            self.dotModel!,
+            color: UIColor.green.cgColor)
+        
+        // Render green dot
+        self.layer.addSublayer(dot!)
+    }
+    
+    func setIncorrect() {
+        // Change pulsator to dark red color
+        pulsator?.backgroundColor = UIColor.darkRed.cgColor
+        
+        // Remove previous dot
+        dot?.removeFromSuperlayer()
+        
+        // Generate dark red dot
+        dot = CAShapeLayer.generateDotWith(
+            self.dotModel!,
+            color: UIColor.darkRed.cgColor)
+        
+        // Render dark red dot
+        self.layer.addSublayer(dot!)
+    }
+    
+    func reset() {
+        // Change pulsator to blue color
+        pulsator?.backgroundColor = UIColor.blue.cgColor
+        
+        // Remove previous dot
+        dot?.removeFromSuperlayer()
+        
+        // Generate blue dot
+        dot = CAShapeLayer.generateDotWith(
+            self.dotModel!,
+            color: UIColor.blue.cgColor)
+        
+        // Render blue dot
+        self.layer.addSublayer(dot!)
+    }
+    
+    func stop() {
+        pulsator?.stop()
+    }
+}
