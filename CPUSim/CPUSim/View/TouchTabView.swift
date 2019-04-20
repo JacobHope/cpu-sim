d45f7be3d6d4dab4c0cf4ee75be8cdde964bf544
Jacob M. Hope <jacobmhope@gmail.com>
2019-04-20 16:31:12 -0500

    Add Touch Tab View.

--- CPUSim/CPUSim/View/TouchTabView.swift
+++ CPUSim/CPUSim/View/TouchTabView.swift
@@ -6,96 +6,77 @@
 //  Copyright © 2019 Jacob M. Hope. All rights reserved.
 //
 
+// Glowing animation based on:
+// https://stackoverflow.com/questions/44850058/need-a-glowing-animation-around-a-button
+
+import UIKit
 import Foundation
 
-class TouchBarView: UIView {
+class TouchTabView: UIView {
+    var duration : CGFloat = 3
+    var cornerRadius : CGFloat = 5
+    var maxGlowSize : CGFloat = 10
+    var minGlowSize : CGFloat = 0
     
-    var name: String = "touchBar"
+    var name: String = "touchTab"
     
-    func setupWith(_ dotModel: DotModel) {
-        self.dotModel = dotModel
-        
-        // Generate a Pulsator instance
-        let pulsator = Pulsator.generatePulsator(radius: self.frame.width, backgroundColor: UIColor.blue.cgColor)
-        
-        // Add Pulsator as a sublayer to TouchPointView
-        self.layer.addSublayer(pulsator)
-        
-        // Start pulsating
-        pulsator.start()
-        
-        // Keep track of pulsator in order to change color, toggle off, etc
-        self.pulsator = pulsator
-        
-        // Setup dot
-        dot = CAShapeLayer.generateDotWith(
-            self.dotModel!,
-            color: UIColor.blue.cgColor)
-        
-        // Render dot
-        self.layer.addSublayer(dot!)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.contentScaleFactor = UIScreen.main.scale
+        self.layer.masksToBounds = false
+        self.setup()
+        self.startAnimation()
     }
     
     func hitTest(_ touch: UITouch, event: UIEvent?) -> UIView? {
-        guard let hitView = self.hitTest(
-            CGPoint(x: touch.location(in: self).x + self.frame.width / 2,
-                    y: touch.location(in: self).y + self.frame.height / 2),
-            with: event)
-            else {
-                return nil
-        }
+        //TODO implement hit test for touch tab
+        let hitView = UIView() //STUB
         return hitView
     }
     
+    func setup() {
+        self.layer.cornerRadius = cornerRadius
+        self.layer.shadowPath = CGPath(roundedRect: self.bounds, cornerWidth: cornerRadius, cornerHeight: cornerRadius, transform: nil)
+        self.layer.shadowColor = UIColor.blue.cgColor
+        self.layer.shadowOffset = CGSize.zero
+        self.layer.shadowRadius = maxGlowSize
+        self.layer.shadowOpacity = 1
+    }
+    
+    func startAnimation() {
+        let layerAnimation = CABasicAnimation(keyPath: "shadowRadius")
+        layerAnimation.fromValue = maxGlowSize
+        layerAnimation.toValue = minGlowSize
+        layerAnimation.autoreverses = true
+        layerAnimation.isAdditive = false
+        layerAnimation.duration = CFTimeInterval(duration/2)
+        layerAnimation.fillMode = CAMediaTimingFillMode.forwards
+        layerAnimation.isRemovedOnCompletion = false
+        layerAnimation.repeatCount = .infinity
+        self.layer.add(layerAnimation, forKey: "glowingAnimation")
+    }
+    
     func setCorrect() {
-        // Change pulsator to green color
-        pulsator?.backgroundColor = UIColor.green.cgColor
-        
-        // Remove previous dot
-        dot?.removeFromSuperlayer()
-        
-        // Generate green dot
-        dot = CAShapeLayer.generateDotWith(
-            self.dotModel!,
-            color: UIColor.green.cgColor)
-        
-        // Render green dot
-        self.layer.addSublayer(dot!)
+        //TODO set tab to green
     }
     
     func setIncorrect() {
-        // Change pulsator to dark red color
-        pulsator?.backgroundColor = UIColor.darkRed.cgColor
-        
-        // Remove previous dot
-        dot?.removeFromSuperlayer()
-        
-        // Generate dark red dot
-        dot = CAShapeLayer.generateDotWith(
-            self.dotModel!,
-            color: UIColor.darkRed.cgColor)
-        
-        // Render dark red dot
-        self.layer.addSublayer(dot!)
+        //TODO set tab to dark red
     }
     
     func reset() {
-        // Change pulsator to blue color
-        pulsator?.backgroundColor = UIColor.blue.cgColor
-        
-        // Remove previous dot
-        dot?.removeFromSuperlayer()
-        
-        // Generate blue dot
-        dot = CAShapeLayer.generateDotWith(
-            self.dotModel!,
-            color: UIColor.blue.cgColor)
-        
-        // Render blue dot
-        self.layer.addSublayer(dot!)
+       //TODO reset tab to blue
     }
     
     func stop() {
-        pulsator?.stop()
+      //TODO stop glow
     }
 }
