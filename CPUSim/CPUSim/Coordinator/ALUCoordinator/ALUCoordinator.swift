e997fb3e5691d41c43915247a6187bba1e727a99
Jacob M. Hope <jacobmhope@gmail.com>
2019-04-24 11:22:03 -0500

    Add ALU coordinator folder and move related code into it.

--- CPUSim/CPUSim/Coordinator/ALUCoordinator/ALUCoordinator.swift
+++ CPUSim/CPUSim/Coordinator/ALUCoordinator/ALUCoordinator.swift
@@ -23,30 +23,38 @@ class ALUCoordinatorPayload {
 class ALUCoordinator: RootViewCoordinator {
     
     // MARK: Properties
-    
+
     var childCoordinators: [Coordinator] = []
     var rootViewController: UIViewController {
         return self.navigationController
     }
     weak var delegate: ALUCoordinatorDelegate?
     var orderPayload: ALUCoordinatorPayload?
-    private lazy var navigationController: UINavigationController = {
+    lazy var navigationController: UINavigationController = {
         let navigationController = UINavigationController()
         return navigationController
     }()
-    
-    private let drawingService: Drawing
-    private let fetchStateService: State
-    private let memoryAccessStateService: State
-    private let writeBackStateService: State
+
+    // MARK: Properties - Drawing State Services
+
+    let drawingService: Drawing
+
+    // MARK: Properties - Instruction State Services
+
+    let fetchStateService: State
+    let decodeStateService: State
+    let memoryAccessStateService: State
+    let writeBackStateService: State
     
     // MARK: Init
     
     init(drawingService: Drawing,
          fetchStateService: State,
+         decodeStateService: State,
          memoryAccessStateService: State,
          writeBackStateService: State) {
         self.drawingService = drawingService
+        self.decodeStateService = decodeStateService
         self.fetchStateService = fetchStateService
         self.memoryAccessStateService = memoryAccessStateService
         self.writeBackStateService = writeBackStateService
@@ -90,212 +98,212 @@ class ALUCoordinator: RootViewCoordinator {
 }
 
 
-// MARK: FetchViewControllerDelegate
-
-extension ALUCoordinator: FetchViewControllerDelegate {
-    
-    func fetchViewControllerOnTouchesBegan(_ fetchViewController: FetchViewController, _ touches: Set<UITouch>, with event: UIEvent?) {
-        drawingService.clearDrawing(
-            imageView: fetchViewController.drawingImageView)
-        
-        fetchStateService.handleTouchesBegan(
-            touches,
-            with: event,
-            touchPoints: fetchViewController.touchPoints,
-            view: fetchViewController.drawingImageView)
-    }
-    
-    func fetchViewControllerOnTouchesMoved(_ fetchViewController: FetchViewController, _ touches: Set<UITouch>, with event: UIEvent?) {
-        // todo: pass in only drawingImageView
-        fetchStateService.handleTouchesMoved(
-            touches,
-            with: event,
-            imageView: fetchViewController.drawingImageView,
-            view: fetchViewController.drawingImageView,
-            withDrawing: drawingService,
-            touchPoints: fetchViewController.touchPoints,
-            lines: fetchViewController.lines)
-    }
-    
-    func fetchViewControllerOnTouchesEnded(_ fetchViewController: FetchViewController) {
-        fetchStateService.resetState()
-    }
-    
-    func fetchViewControllerOnTouchesCancelled(_ fetchViewController: FetchViewController) {
-        drawingService.resumeTouchInput()
-        fetchStateService.resetState()
-    }
-    
-    func fetchViewControllerClearDrawing(_ fetchViewController: FetchViewController) {
-        drawingService.clearDrawing(imageView: fetchViewController.drawingImageView)
-    }
-    
-    func fetchViewControllerSetup(_ fetchViewController: FetchViewController) {
-        // Setup ProgressView
-        fetchViewController.progressView.progress = 0.0
-        fetchViewController.progressView.progressTintColor = UIColor.green
-        
-        // Setup event subscribers
-        SwiftEventBus.onMainThread(fetchViewController, name: Events.aluFetchOnCorrect) { result in
-            let progress: Float = result?.object as! Float
-            fetchViewController.progressView.setProgress(progress, animated: true)
-            
-            // If progress is complete...
-            if (progress == 1) {
-                // ...show complete button and animate tab bar after 4 seconds
-                DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
-                    // Unhide complete button
-                    fetchViewController.completeButton.isHidden = false
-                    
-                    // Begin complete button animation
-                    UIView.beginCompleteButtonAnimation(fetchViewController.completeButton)
-                    
-                    // Begin tab bar animation
-                    fetchViewController.ifIdTab.setStageFinished()
-                }
-            }
-        }
-        
-        // Setup complete button
-        fetchViewController.completeButton.isHidden = true
-        
-        // Setup TouchPointViews
-        fetchViewController.ifMuxToPcStart.name = TouchPointNames.ifMuxToPcStart
-        fetchViewController.ifMuxToPcEnd.name = TouchPointNames.ifMuxToPcEnd
-        fetchViewController.ifPcToAluStart.name = TouchPointNames.ifPcToAluStart
-        fetchViewController.ifPcToAluEnd.name = TouchPointNames.ifPcToAluEnd
-        fetchViewController.ifPcToImEnd.name = TouchPointNames.ifPcToImEnd
-        fetchViewController.ifFourToAluStart.name = TouchPointNames.ifFourToAluStart
-        fetchViewController.ifFourToAluEnd.name = TouchPointNames.ifFourToAluEnd
-        fetchViewController.ifAluToMuxStart.name = TouchPointNames.ifAluToMuxStart
-        fetchViewController.ifAluToMuxEnd.name = TouchPointNames.ifAluToMuxEnd
-        fetchViewController.ifImToIdStart.name = TouchPointNames.ifImToIdStart
-        fetchViewController.ifImToIdEnd.name = TouchPointNames.ifImToIdEnd
-        fetchViewController.ifAluToIdEnd.name = TouchPointNames.ifAluToIdEnd
-        
-        fetchViewController.touchPoints = [
-            fetchViewController.ifMuxToPcStart,
-            fetchViewController.ifMuxToPcEnd,
-            fetchViewController.ifPcToAluStart,
-            fetchViewController.ifPcToAluEnd,
-            fetchViewController.ifPcToImEnd,
-            fetchViewController.ifFourToAluStart,
-            fetchViewController.ifFourToAluEnd,
-            fetchViewController.ifAluToMuxStart,
-            fetchViewController.ifAluToMuxEnd,
-            fetchViewController.ifImToIdStart,
-            fetchViewController.ifImToIdEnd,
-            fetchViewController.ifAluToIdEnd
-        ]
-        
-        // Setup lines
-        
-        // IFMUXtoPC
-        fetchViewController.lines[TouchPointNames.ifMuxToPcEnd] = [
-            fetchViewController.ifMuxToPc1,
-            fetchViewController.ifMuxToPc2,
-            fetchViewController.ifMuxToPc3
-        ]
-        
-        // IFPCtoALU
-        fetchViewController.lines[TouchPointNames.ifPcToAluEnd] = [
-            fetchViewController.ifPcToAlu1,
-            fetchViewController.ifPcToAlu2
-        ]
-        
-        // IFPCtoIM
-        fetchViewController.lines[TouchPointNames.ifPcToImEnd] = [
-            fetchViewController.ifPcToIm1
-        ]
-        
-        // IFFourToALU
-        fetchViewController.lines[TouchPointNames.ifFourToAluEnd] = [
-            fetchViewController.ifFourToAlu1
-        ]
-        
-        // IFALUToMUX
-        fetchViewController.lines[TouchPointNames.ifAluToMuxEnd] = [
-            fetchViewController.ifAluToMux1,
-            fetchViewController.ifAluToMux2,
-            fetchViewController.ifAluToMux3
-        ]
-        
-        // IFIMtoID
-        fetchViewController.lines[TouchPointNames.ifImToIdEnd] = [
-            fetchViewController.ifImToId1
-        ]
-        
-        // IFALUToID
-        fetchViewController.lines[TouchPointNames.ifAluToIdEnd] = [
-            fetchViewController.ifAluToId1
-        ]
-        
-        // Setup all touch points
-        fetchViewController.touchPoints.forEach { touchPoint in
-            touchPoint.setupWith(DotModel.defaultDotModel())
-        }
-        
-        // Setup all lines
-        for (_, v) in fetchViewController.lines {
-            v.forEach { line in
-                line.setup()
-            }
-        }
-    }
-    
-    func fetchViewControllerDidSwipeLeft(_ fetchViewController: FetchViewController) {
-        if (!fetchStateService.isDrawing) {
-            self.navigationController.popViewController(animated: true)
-        }
-    }
-    
-    func fetchViewControllerDidSwipeRight(_ fetchViewController: FetchViewController) {
-        if (!fetchStateService.isDrawing) {
-            // Hide complete button
-            fetchViewController.completeButton.isHidden = true
-            self.showDecodeViewController()
-        }
-    }
-    
-    func fetchViewControllerDidTapClose(_ fetchViewController: FetchViewController) {
-        self.delegate?.aluCoordinatorDidRequestCancel(aluCoordinator: self)
-    }
-    
-    func fetchViewController(_ fetchViewController: FetchViewController) {
-        //self.showDecodeViewController()
-    }
-}
+//// MARK: FetchViewControllerDelegate
+//
+//extension ALUCoordinator: FetchViewControllerDelegate {
+//
+//    func fetchViewControllerOnTouchesBegan(_ fetchViewController: FetchViewController, _ touches: Set<UITouch>, with event: UIEvent?) {
+//        drawingService.clearDrawing(
+//            imageView: fetchViewController.drawingImageView)
+//
+//        fetchStateService.handleTouchesBegan(
+//            touches,
+//            with: event,
+//            touchPoints: fetchViewController.touchPoints,
+//            view: fetchViewController.drawingImageView)
+//    }
+//
+//    func fetchViewControllerOnTouchesMoved(_ fetchViewController: FetchViewController, _ touches: Set<UITouch>, with event: UIEvent?) {
+//        // todo: pass in only drawingImageView
+//        fetchStateService.handleTouchesMoved(
+//            touches,
+//            with: event,
+//            imageView: fetchViewController.drawingImageView,
+//            view: fetchViewController.drawingImageView,
+//            withDrawing: drawingService,
+//            touchPoints: fetchViewController.touchPoints,
+//            lines: fetchViewController.lines)
+//    }
+//
+//    func fetchViewControllerOnTouchesEnded(_ fetchViewController: FetchViewController) {
+//        fetchStateService.resetState()
+//    }
+//
+//    func fetchViewControllerOnTouchesCancelled(_ fetchViewController: FetchViewController) {
+//        drawingService.resumeTouchInput()
+//        fetchStateService.resetState()
+//    }
+//
+//    func fetchViewControllerClearDrawing(_ fetchViewController: FetchViewController) {
+//        drawingService.clearDrawing(imageView: fetchViewController.drawingImageView)
+//    }
+//
+//    func fetchViewControllerSetup(_ fetchViewController: FetchViewController) {
+//        // Setup ProgressView
+//        fetchViewController.progressView.progress = 0.0
+//        fetchViewController.progressView.progressTintColor = UIColor.green
+//
+//        // Setup event subscribers
+//        SwiftEventBus.onMainThread(fetchViewController, name: Events.aluFetchOnCorrect) { result in
+//            let progress: Float = result?.object as! Float
+//            fetchViewController.progressView.setProgress(progress, animated: true)
+//
+//            // If progress is complete...
+//            if (progress == 1) {
+//                // ...show complete button and animate tab bar after 4 seconds
+//                DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+//                    // Unhide complete button
+//                    fetchViewController.completeButton.isHidden = false
+//
+//                    // Begin complete button animation
+//                    UIView.beginCompleteButtonAnimation(fetchViewController.completeButton)
+//
+//                    // Begin tab bar animation
+//                    fetchViewController.ifIdTab.setStageFinished()
+//                }
+//            }
+//        }
+//
+//        // Setup complete button
+//        fetchViewController.completeButton.isHidden = true
+//
+//        // Setup TouchPointViews
+//        fetchViewController.ifMuxToPcStart.name = TouchPointNames.ifMuxToPcStart
+//        fetchViewController.ifMuxToPcEnd.name = TouchPointNames.ifMuxToPcEnd
+//        fetchViewController.ifPcToAluStart.name = TouchPointNames.ifPcToAluStart
+//        fetchViewController.ifPcToAluEnd.name = TouchPointNames.ifPcToAluEnd
+//        fetchViewController.ifPcToImEnd.name = TouchPointNames.ifPcToImEnd
+//        fetchViewController.ifFourToAluStart.name = TouchPointNames.ifFourToAluStart
+//        fetchViewController.ifFourToAluEnd.name = TouchPointNames.ifFourToAluEnd
+//        fetchViewController.ifAluToMuxStart.name = TouchPointNames.ifAluToMuxStart
+//        fetchViewController.ifAluToMuxEnd.name = TouchPointNames.ifAluToMuxEnd
+//        fetchViewController.ifImToIdStart.name = TouchPointNames.ifImToIdStart
+//        fetchViewController.ifImToIdEnd.name = TouchPointNames.ifImToIdEnd
+//        fetchViewController.ifAluToIdEnd.name = TouchPointNames.ifAluToIdEnd
+//
+//        fetchViewController.touchPoints = [
+//            fetchViewController.ifMuxToPcStart,
+//            fetchViewController.ifMuxToPcEnd,
+//            fetchViewController.ifPcToAluStart,
+//            fetchViewController.ifPcToAluEnd,
+//            fetchViewController.ifPcToImEnd,
+//            fetchViewController.ifFourToAluStart,
+//            fetchViewController.ifFourToAluEnd,
+//            fetchViewController.ifAluToMuxStart,
+//            fetchViewController.ifAluToMuxEnd,
+//            fetchViewController.ifImToIdStart,
+//            fetchViewController.ifImToIdEnd,
+//            fetchViewController.ifAluToIdEnd
+//        ]
+//
+//        // Setup lines
+//
+//        // IFMUXtoPC
+//        fetchViewController.lines[TouchPointNames.ifMuxToPcEnd] = [
+//            fetchViewController.ifMuxToPc1,
+//            fetchViewController.ifMuxToPc2,
+//            fetchViewController.ifMuxToPc3
+//        ]
+//
+//        // IFPCtoALU
+//        fetchViewController.lines[TouchPointNames.ifPcToAluEnd] = [
+//            fetchViewController.ifPcToAlu1,
+//            fetchViewController.ifPcToAlu2
+//        ]
+//
+//        // IFPCtoIM
+//        fetchViewController.lines[TouchPointNames.ifPcToImEnd] = [
+//            fetchViewController.ifPcToIm1
+//        ]
+//
+//        // IFFourToALU
+//        fetchViewController.lines[TouchPointNames.ifFourToAluEnd] = [
+//            fetchViewController.ifFourToAlu1
+//        ]
+//
+//        // IFALUToMUX
+//        fetchViewController.lines[TouchPointNames.ifAluToMuxEnd] = [
+//            fetchViewController.ifAluToMux1,
+//            fetchViewController.ifAluToMux2,
+//            fetchViewController.ifAluToMux3
+//        ]
+//
+//        // IFIMtoID
+//        fetchViewController.lines[TouchPointNames.ifImToIdEnd] = [
+//            fetchViewController.ifImToId1
+//        ]
+//
+//        // IFALUToID
+//        fetchViewController.lines[TouchPointNames.ifAluToIdEnd] = [
+//            fetchViewController.ifAluToId1
+//        ]
+//
+//        // Setup all touch points
+//        fetchViewController.touchPoints.forEach { touchPoint in
+//            touchPoint.setupWith(DotModel.defaultDotModel())
+//        }
+//
+//        // Setup all lines
+//        for (_, v) in fetchViewController.lines {
+//            v.forEach { line in
+//                line.setup()
+//            }
+//        }
+//    }
+//
+//    func fetchViewControllerDidSwipeLeft(_ fetchViewController: FetchViewController) {
+//        if (!fetchStateService.isDrawing) {
+//            self.navigationController.popViewController(animated: true)
+//        }
+//    }
+//
+//    func fetchViewControllerDidSwipeRight(_ fetchViewController: FetchViewController) {
+//        if (!fetchStateService.isDrawing) {
+//            // Hide complete button
+//            fetchViewController.completeButton.isHidden = true
+//            self.showDecodeViewController()
+//        }
+//    }
+//
+//    func fetchViewControllerDidTapClose(_ fetchViewController: FetchViewController) {
+//        self.delegate?.aluCoordinatorDidRequestCancel(aluCoordinator: self)
+//    }
+//
+//    func fetchViewController(_ fetchViewController: FetchViewController) {
+//        //self.showDecodeViewController()
+//    }
+//}
 
 // MARK: DecodeViewControllerDelegate
 
-extension ALUCoordinator: DecodeViewControllerDelegate {
-    func decodeViewControllerDidSwipeLeft(_ decodeViewController: DecodeViewController) {
-        self.navigationController.popViewController(animated: true)
-
-        // Reset animations
-        guard let tvc = self.navigationController.topViewController else {
-            return
-        }
-        if (tvc.nibName == "FetchView") {
-            let fvc: FetchViewController = tvc as! FetchViewController
-            fvc.touchPoints.forEach { tp in
-                if (!tp.isHidden) {
-                    tp.setupWith(DotModel.defaultDotModel())
-                    
-                }
-            }
-        }
-    }
-    
-    func decodeViewControllerDidSwipeRight(_ decodeViewController: DecodeViewController) {
-        self.showExecuteViewController()
-    }
-    
-    func decodeViewController(_ decodeViewController: DecodeViewController) {
-        
-    }
-    
-}
+//extension ALUCoordinator: DecodeViewControllerDelegate {
+//    func decodeViewControllerDidSwipeLeft(_ decodeViewController: DecodeViewController) {
+//        self.navigationController.popViewController(animated: true)
+//
+//        // Reset animations
+//        guard let tvc = self.navigationController.topViewController else {
+//            return
+//        }
+//        if (tvc.nibName == "FetchView") {
+//            let fvc: FetchViewController = tvc as! FetchViewController
+//            fvc.touchPoints.forEach { tp in
+//                if (!tp.isHidden) {
+//                    tp.setupWith(DotModel.defaultDotModel())
+//
+//                }
+//            }
+//        }
+//    }
+//
+//    func decodeViewControllerDidSwipeRight(_ decodeViewController: DecodeViewController) {
+//        self.showExecuteViewController()
+//    }
+//
+//    func decodeViewController(_ decodeViewController: DecodeViewController) {
+//
+//    }
+//
+//}
 
 // MARK: ExecuteViewControllerDelegate
 
